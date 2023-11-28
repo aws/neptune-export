69b05a999345fea8e6e60758cbf1c4d4cb2edbf0
Cole-Greer <Cole.Greer@improving.com>
2023-11-28 13:40:59 -0800

    adds additional tests

--- src/test/java/com/amazonaws/services/neptune/ExportServiceIntegrationTest.java
+++ src/test/java/com/amazonaws/services/neptune/ExportServiceIntegrationTest.java
@@ -33,6 +33,40 @@ public class ExportServiceIntegrationTest extends AbstractExportIntegrationTest{
     }
 
     @Test
+    public void testExportPgML() {
+        exit.expectSystemExitWithStatus(0);
+        exit.checkAssertionAfterwards(new EquivalentResultsAssertion("src/test/resources/IntegrationTest/testExportPgWithEdgeAndVertexLabelsWithoutTypes"));
+        exit.checkAssertionAfterwards(new EquivalentTrainingConfigAssertion("src/test/resources/IntegrationTest/ml-training-data-configs/v2.json"));
+
+        final String[] command = {
+                "nesvc",
+                "--root-path", outputDir.getPath(),
+                "--json", "{\n" +
+                "        \"command\": \"export-pg\",\n" +
+                "        \"params\": {\n" +
+                "          \"endpoint\": \""+neptuneEndpoint+"\",\n" +
+                "          \"profile\": \"neptune_ml\"\n" +
+                "        },\n" +
+                "        \"additionalParams\": {\n" +
+                "          \"neptune_ml\": {\n" +
+                "            \"version\": \"v2.0\",\n" +
+                "            \"targets\": [\n" +
+                "              {\n" +
+                "                \"node\": \"Airport\",\n" +
+                "                \"property\": \"city\",\n" +
+                "                \"type\": \"classification\"\n" +
+                "              }\n" +
+                "            ]\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }"+
+                "}"
+        };
+        final NeptuneExportRunner runner = new NeptuneExportRunner(command);
+        runner.run();
+    }
+
+    @Test
     public void testExportPgFromQueriesML() {
         exit.expectSystemExitWithStatus(0);
         exit.checkAssertionAfterwards(new EquivalentResultsAssertion("src/test/resources/IntegrationTest/testExportPgWithEdgeAndVertexLabelsWithoutTypes"));
