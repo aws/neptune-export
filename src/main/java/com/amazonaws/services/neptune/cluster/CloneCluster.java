5b9421f49d389cd00d97b1e85e4fa6c830f3b802
Cole-Greer <Cole.Greer@improving.com>
2024-07-11 12:24:01 -0700

    Configure export to use AWS Batch jobid as export id when available

--- src/main/java/com/amazonaws/services/neptune/cluster/CloneCluster.java
+++ src/main/java/com/amazonaws/services/neptune/cluster/CloneCluster.java
@@ -13,6 +13,7 @@ permissions and limitations under the License.
 package com.amazonaws.services.neptune.cluster;
 
 import com.amazonaws.services.neptune.AmazonNeptune;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.UUID;
 import java.util.function.Supplier;
@@ -51,7 +52,11 @@ public class CloneCluster implements CloneClusterStrategy {
         }
 
         String clusterId = originalClusterMetadata.clusterId();
-        String targetClusterId = String.format("neptune-export-cluster-%s", UUID.randomUUID().toString().substring(0, 5));
+        String targetClusterIdSuffix = System.getenv("AWS_BATCH_JOB_ID"); // Use AWS Batch job id if running in Neptune Export Service
+        if (StringUtils.isEmpty(targetClusterIdSuffix)) {
+            targetClusterIdSuffix = UUID.randomUUID().toString().substring(0, 5);
+        }
+        String targetClusterId = String.format("neptune-export-cluster-%s", targetClusterIdSuffix);
 
         AddCloneTask addCloneTask = new AddCloneTask(
                 clusterId,
