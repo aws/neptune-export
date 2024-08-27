f4e365131e08ac693c37fe46f7d9772c3399b854
Cole Greer <112986082+Cole-Greer@users.noreply.github.com>
2024-08-27 16:02:37 -0700

    Update errors in S3 Event Handler (#135)
    In cases where the local export files cannot be found by the ExportToS3NeptuneExportEventHandler,
    update the logic to throw an exception where previously a warning was simply logged and the upload
    skipped. The previous logic is viewing the S3 upload as an optional, non-integral part of the export
    task. This leads to issues in deployments such as the Neptune Export Service in which the local files
    are temporary and lost once the job is complete. In cases such as these, the S3 upload is a critical.
    
    The update to throw an error in this case will prevent deployments such as the Neptune Export Service
    from terminating with a successful status in cases where the S3 upload was skipped.

---
+++ src/test/java/com/amazonaws/services/neptune/export/ExportToS3NeptuneExportEventHandlerTest.java
@@ -0,0 +1,59 @@
+/*
+Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+    http://www.apache.org/licenses/LICENSE-2.0
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+package com.amazonaws.services.neptune.export;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.neptune.cluster.Cluster;
+import com.amazonaws.services.neptune.io.Directories;
+import com.amazonaws.services.neptune.propertygraph.ExportStats;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ExportToS3NeptuneExportEventHandlerTest {
+
+    @Test
+    public void shouldThrowErrorIfDirectoryMissing() {
+        ExportToS3NeptuneExportEventHandler handler = new ExportToS3NeptuneExportEventHandler(
+                "localOutputPath",
+                "outputS3Path",
+                "s3Region",
+                "completionFileS3Path",
+                mock(ObjectNode.class),
+                false,
+                new ExportToS3NeptuneExportEventHandler.S3UploadParams(),
+                Collections.EMPTY_SET,
+                Collections.EMPTY_SET,
+                "",
+                mock(AWSCredentialsProvider.class)
+        );
+
+        Directories mockDir = mock(Directories.class);
+        when(mockDir.rootDirectory()).thenReturn(Paths.get("nonExistentDirectory"));
+
+        Throwable t = assertThrows(RuntimeException.class, () -> handler.onExportComplete(
+                mockDir, mock(ExportStats.class), mock(Cluster.class)
+        ));
+        assertEquals(
+                "Failed to upload files to S3 because upload directory from which to upload files does not exist",
+                t.getMessage()
+        );
+    }
+}
