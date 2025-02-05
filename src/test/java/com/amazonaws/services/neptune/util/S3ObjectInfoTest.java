2c25b38c025cf41913516c4830289bfa5f9a81cc
Cole-Greer <Cole.Greer@improving.com>
2025-02-04 17:13:14 -0800

    add back missing test classes

--- src/test/java/com/amazonaws/services/neptune/util/S3ObjectInfoTest.java
+++ src/test/java/com/amazonaws/services/neptune/util/S3ObjectInfoTest.java
@@ -1,193 +1,195 @@
-///*
-//Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//Licensed under the Apache License, Version 2.0 (the "License").
-//You may not use this file except in compliance with the License.
-//A copy of the License is located at
-//    http://www.apache.org/licenses/LICENSE-2.0
-//or in the "license" file accompanying this file. This file is distributed
-//on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-//express or implied. See the License for the specific language governing
-//permissions and limitations under the License.
-//*/
-//
-//package com.amazonaws.services.neptune.util;
-//
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNull;
-//
-//public class S3ObjectInfoTest {
-//
-//    @Test
-//    public void canParseBucketFromURI(){
-//        String s3Uri = "s3://my-bucket/a/b/c";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("my-bucket", s3ObjectInfo.bucket());
-//    }
-//
-//    @Test
-//    public void canParseKeyWithoutTrailingSlashFromURI(){
-//        String s3Uri = "s3://my-bucket/a/b/c";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/c", s3ObjectInfo.key());
-//    }
-//
-//    @Test
-//    public void canParseKeyWithTrainlingSlashFromURI(){
-//        String s3Uri = "s3://my-bucket/a/b/c/";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/c/", s3ObjectInfo.key());
-//    }
-//
-//    @Test
-//    public void canCreateDownloadFileForKeyWithoutTrailingSlash(){
-//        String s3Uri = "s3://my-bucket/a/b/c.txt";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("/temp/c.txt", s3ObjectInfo.createDownloadFile("/temp").getAbsolutePath());
-//    }
-//
-//    @Test
-//    public void canCreateDownloadFileForKeyWithTrailingSlash(){
-//        String s3Uri = "s3://my-bucket/a/b/c/";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("/temp/c", s3ObjectInfo.createDownloadFile("/temp").getAbsolutePath());
-//    }
-//
-//    @Test
-//    public void canCreateNewInfoForKeyWithoutTrailingSlash() {
-//        String s3Uri = "s3://my-bucket/a/b/c";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/c/dir", s3ObjectInfo.withNewKeySuffix("dir").key());
-//    }
-//
-//    @Test
-//    public void canCreateNewKeyForKeyWithTrailingSlash() {
-//        String s3Uri = "s3://my-bucket/a/b/c/";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/c/dir", s3ObjectInfo.withNewKeySuffix("dir").key());
-//    }
-//
-//    @Test
-//    public void canReplacePlaceholderInKey() {
-//        String s3Uri = "s3://my-bucket/a/b/_COMPLETION_ID_/manifest.json";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/123/manifest.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
-//    }
-//
-//    @Test
-//    public void canReplaceTmpPlaceholderInKey() {
-//        String s3Uri = "s3://my-bucket/a/b/tmp/manifest.json";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/failed/manifest.json", s3ObjectInfo.replaceOrAppendKey("/tmp/", "/failed/").key());
-//    }
-//
-//    @Test
-//    public void canAppendSuffixIfNoPlaceholder() {
-//        String s3Uri = "s3://my-bucket/a/b/";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/123", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
-//    }
-//
-//    @Test
-//    public void canAppendAltSuffixIfNoPlaceholder() {
-//        String s3Uri = "s3://my-bucket/a/b/";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("a/b/123.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123", "123.json").key());
-//    }
-//
-//    @Test
-//    public void canHandlePathsWithBucketNameOnlyNoSlash(){
-//        String s3Uri = "s3://my-bucket";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("", s3ObjectInfo.key());
-//        assertEquals("s3://my-bucket/new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").toString());
-//        assertEquals("new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").key());
-//        assertEquals("/123", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
-//        assertEquals("/123.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123", "123.json").key());
-//    }
-//
-//    @Test
-//    public void canHandlePathsWithBucketNameWithSlash(){
-//        String s3Uri = "s3://my-bucket/";
-//
-//        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
-//
-//        assertEquals("", s3ObjectInfo.key());
-//        assertEquals("s3://my-bucket/new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").toString());
-//        assertEquals("new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").key());
-//        assertEquals("/123", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
-//        assertEquals("/123.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123", "123.json").key());
-//    }
-//
-//    @Test
-//    public void canSetContentLengthAndDefaultEncryptionTypeProperlyWithEmptyKey(){
-//        long testLength = 100;
-//        String testKeyId = "";
-//
-//        ObjectMetadata objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, testKeyId);
-//
-//        assertEquals(testLength, objectMetadata.getContentLength());
-//        assertEquals(SSEAlgorithm.AES256.getAlgorithm(), objectMetadata.getSSEAlgorithm());
-//        assertNull(objectMetadata.getSSEAwsKmsKeyId());
-//    }
-//
-//    @Test
-//    public void canSetContentLengthAndDefaultEncryptionTypeProperlyWithBlankKey(){
-//        long testLength = 100;
-//        String testKeyId = "   ";
-//
-//        ObjectMetadata objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, testKeyId);
-//
-//        assertEquals(testLength, objectMetadata.getContentLength());
-//        assertEquals(SSEAlgorithm.AES256.getAlgorithm(), objectMetadata.getSSEAlgorithm());
-//        assertNull(objectMetadata.getSSEAwsKmsKeyId());
-//    }
-//
-//    @Test
-//    public void canSetContentLengthAndDefaultEncryptionTypeProperlyWithNullKey(){
-//        long testLength = 100;
-//
-//        ObjectMetadata objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, null);
-//
-//        assertEquals(testLength, objectMetadata.getContentLength());
-//        assertEquals(SSEAlgorithm.AES256.getAlgorithm(), objectMetadata.getSSEAlgorithm());
-//        assertNull(objectMetadata.getSSEAwsKmsKeyId());
-//    }
-//
-//    @Test
-//    public void canSetContentLengthAndKmsEncryptionTypeProperlyWithCmkKey(){
-//        long testLength = 100;
-//        String testKeyId = "abcdefgh-hijk-0123-4567-0123456789ab";
-//
-//        ObjectMetadata objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, testKeyId);
-//
-//        assertEquals(testLength, objectMetadata.getContentLength());
-//        assertEquals(SSEAlgorithm.KMS.getAlgorithm(), objectMetadata.getSSEAlgorithm());
-//        assertEquals(testKeyId, objectMetadata.getSSEAwsKmsKeyId());
-//    }
-//}
+/*
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+    http://www.apache.org/licenses/LICENSE-2.0
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+package com.amazonaws.services.neptune.util;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class S3ObjectInfoTest {
+
+    @Test
+    public void canParseBucketFromURI(){
+        String s3Uri = "s3://my-bucket/a/b/c";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("my-bucket", s3ObjectInfo.bucket());
+    }
+
+    @Test
+    public void canParseKeyWithoutTrailingSlashFromURI(){
+        String s3Uri = "s3://my-bucket/a/b/c";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/c", s3ObjectInfo.key());
+    }
+
+    @Test
+    public void canParseKeyWithTrainlingSlashFromURI(){
+        String s3Uri = "s3://my-bucket/a/b/c/";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/c/", s3ObjectInfo.key());
+    }
+
+    @Test
+    public void canCreateDownloadFileForKeyWithoutTrailingSlash(){
+        String s3Uri = "s3://my-bucket/a/b/c.txt";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("/temp/c.txt", s3ObjectInfo.createDownloadFile("/temp").getAbsolutePath());
+    }
+
+    @Test
+    public void canCreateDownloadFileForKeyWithTrailingSlash(){
+        String s3Uri = "s3://my-bucket/a/b/c/";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("/temp/c", s3ObjectInfo.createDownloadFile("/temp").getAbsolutePath());
+    }
+
+    @Test
+    public void canCreateNewInfoForKeyWithoutTrailingSlash() {
+        String s3Uri = "s3://my-bucket/a/b/c";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/c/dir", s3ObjectInfo.withNewKeySuffix("dir").key());
+    }
+
+    @Test
+    public void canCreateNewKeyForKeyWithTrailingSlash() {
+        String s3Uri = "s3://my-bucket/a/b/c/";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/c/dir", s3ObjectInfo.withNewKeySuffix("dir").key());
+    }
+
+    @Test
+    public void canReplacePlaceholderInKey() {
+        String s3Uri = "s3://my-bucket/a/b/_COMPLETION_ID_/manifest.json";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/123/manifest.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
+    }
+
+    @Test
+    public void canReplaceTmpPlaceholderInKey() {
+        String s3Uri = "s3://my-bucket/a/b/tmp/manifest.json";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/failed/manifest.json", s3ObjectInfo.replaceOrAppendKey("/tmp/", "/failed/").key());
+    }
+
+    @Test
+    public void canAppendSuffixIfNoPlaceholder() {
+        String s3Uri = "s3://my-bucket/a/b/";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/123", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
+    }
+
+    @Test
+    public void canAppendAltSuffixIfNoPlaceholder() {
+        String s3Uri = "s3://my-bucket/a/b/";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("a/b/123.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123", "123.json").key());
+    }
+
+    @Test
+    public void canHandlePathsWithBucketNameOnlyNoSlash(){
+        String s3Uri = "s3://my-bucket";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("", s3ObjectInfo.key());
+        assertEquals("s3://my-bucket/new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").toString());
+        assertEquals("new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").key());
+        assertEquals("/123", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
+        assertEquals("/123.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123", "123.json").key());
+    }
+
+    @Test
+    public void canHandlePathsWithBucketNameWithSlash(){
+        String s3Uri = "s3://my-bucket/";
+
+        S3ObjectInfo s3ObjectInfo = new S3ObjectInfo(s3Uri);
+
+        assertEquals("", s3ObjectInfo.key());
+        assertEquals("s3://my-bucket/new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").toString());
+        assertEquals("new-suffix", s3ObjectInfo.withNewKeySuffix("new-suffix").key());
+        assertEquals("/123", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123").key());
+        assertEquals("/123.json", s3ObjectInfo.replaceOrAppendKey("_COMPLETION_ID_", "123", "123.json").key());
+    }
+
+    @Test
+    public void canSetContentLengthAndDefaultEncryptionTypeProperlyWithEmptyKey(){
+        long testLength = 100;
+        String testKeyId = "";
+
+        Map<String, String> objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, testKeyId);
+
+        assertEquals(testLength, Long.parseLong(objectMetadata.get("Content-Length")));
+        assertEquals("AES256", objectMetadata.get("x-amz-server-side-encryption"));
+        assertNull(objectMetadata.get("x-amz-server-side-encryption-aws-kms-key-id"));
+    }
+
+    @Test
+    public void canSetContentLengthAndDefaultEncryptionTypeProperlyWithBlankKey(){
+        long testLength = 100;
+        String testKeyId = "   ";
+
+        Map<String, String> objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, testKeyId);
+
+        assertEquals(testLength, Long.parseLong(objectMetadata.get("Content-Length")));
+        assertEquals("AES256", objectMetadata.get("x-amz-server-side-encryption"));
+        assertNull(objectMetadata.get("x-amz-server-side-encryption-aws-kms-key-id"));
+    }
+
+    @Test
+    public void canSetContentLengthAndDefaultEncryptionTypeProperlyWithNullKey(){
+        long testLength = 100;
+
+        Map<String, String> objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, null);
+
+        assertEquals(testLength, Long.parseLong(objectMetadata.get("Content-Length")));
+        assertEquals("AES256", objectMetadata.get("x-amz-server-side-encryption"));
+        assertNull(objectMetadata.get("x-amz-server-side-encryption-aws-kms-key-id"));
+    }
+
+    @Test
+    public void canSetContentLengthAndKmsEncryptionTypeProperlyWithCmkKey(){
+        long testLength = 100;
+        String testKeyId = "abcdefgh-hijk-0123-4567-0123456789ab";
+
+        Map<String, String> objectMetadata = S3ObjectInfo.createObjectMetadata(testLength, testKeyId);
+
+        assertEquals(testLength, Long.parseLong(objectMetadata.get("Content-Length")));
+        assertEquals("aws:kms", objectMetadata.get("x-amz-server-side-encryption"));
+        assertEquals(testKeyId, objectMetadata.get("x-amz-server-side-encryption-aws-kms-key-id"));
+    }
+}
--- src/test/java/com/amazonaws/services/neptune/util/TransferManagerWrapperTest.java
+++ src/test/java/com/amazonaws/services/neptune/util/TransferManagerWrapperTest.java
@@ -1,54 +1,60 @@
-///*
-//Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//Licensed under the Apache License, Version 2.0 (the "License").
-//You may not use this file except in compliance with the License.
-//A copy of the License is located at
-//    http://www.apache.org/licenses/LICENSE-2.0
-//or in the "license" file accompanying this file. This file is distributed
-//on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-//express or implied. See the License for the specific language governing
-//permissions and limitations under the License.
-//*/
-//
-//package com.amazonaws.services.neptune.util;
-//
-//import com.amazonaws.SdkClientException;
-//import com.amazonaws.auth.AnonymousAWSCredentials;
-//import org.junit.Test;
-//import org.mockito.internal.verification.AtLeast;
-//import software.amazon.awssdk.auth.credentials.AwsCredentials;
-//import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-//
-//import static org.junit.Assert.assertNotNull;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class TransferManagerWrapperTest {
-//
-//    private final String REGION = "us-west-2";
-//    @Test
-//    public void shouldHandleNullCredentialsProvider() {
-//        TransferManagerWrapper wrapper = new TransferManagerWrapper(REGION, null);
-//        assertNotNull(wrapper);
-//        assertNotNull(wrapper.get());
-//    }
-//
-//    @Test
-//    public void shouldUseProvidedCredentials() {
-//        AwsCredentialsProvider mockCredentialsProvider = mock(AwsCredentialsProvider.class);
-//        when(mockCredentialsProvider.resolveCredentials()).thenReturn(mock(AwsCredentials.class));
-//
-//        TransferManagerWrapper wrapper = new TransferManagerWrapper(REGION, mockCredentialsProvider);
-//        assertNotNull(wrapper);
-//        assertNotNull(wrapper.get());
-//
-//        //Expected to fail due to invalid credentials. This call is here to force the S3 client to call getCredentials()
-//        try {
-//            wrapper.get().getAmazonS3Client().listBuckets();
-//        }
-//        catch (SdkClientException e) {}
-//
-//        verify(mockCredentialsProvider, new AtLeast(1)).getCredentials();
-//    }
-//}
+/*
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+    http://www.apache.org/licenses/LICENSE-2.0
+or in the "license" file accompanying this file. This file is distributed
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the License for the specific language governing
+permissions and limitations under the License.
+*/
+
+package com.amazonaws.services.neptune.util;
+
+import com.amazonaws.SdkClientException;
+import org.junit.Test;
+import org.mockito.internal.verification.AtLeast;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
+
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+public class TransferManagerWrapperTest {
+
+    private final String REGION = "us-west-2";
+    @Test
+    public void shouldHandleNullCredentialsProvider() {
+        TransferManagerWrapper wrapper = new TransferManagerWrapper(REGION, null);
+        assertNotNull(wrapper);
+        assertNotNull(wrapper.get());
+    }
+
+    @Test
+    public void shouldUseProvidedCredentials() {
+        AwsCredentialsProvider mockCredentialsProvider = spy(AnonymousCredentialsProvider.class);
+
+        TransferManagerWrapper wrapper = new TransferManagerWrapper(REGION, mockCredentialsProvider);
+        assertNotNull(wrapper);
+        assertNotNull(wrapper.get());
+
+        //Expected to fail due to invalid credentials. This call is here to force the S3 client to call getCredentials()
+        try {
+            wrapper.get().downloadFile(
+                    DownloadFileRequest.builder()
+                            .destination(Paths.get("filepath"))
+                            .getObjectRequest(GetObjectRequest.builder().bucket("NonExistentBucket4589634587645").key("test").build())
+                            .build()
+            );
+        }
+        catch (SdkClientException e) {}
+
+        verify(mockCredentialsProvider, new AtLeast(1)).resolveCredentials();
+    }
+}
